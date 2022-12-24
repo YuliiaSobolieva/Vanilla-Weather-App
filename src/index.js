976d816529e4a9f703cce96a898d93b407966ef2
YuliiaSobolieva <115546546+YuliiaSobolieva@users.noreply.github.com>
2022-12-24 19:18:49 +0100

    Added day and time for user lication only
    next steps:
    4. images
    5. forecast for next 5 days

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // using search - submit (buttun Search or enter)
+
 let cityInput;
 let currentCity;
 let currentTemp;
+let currentTime;
+let currentWeekDay;
 let cityValue = "";
-// let cityLoc = "";
 let units = "metric";
 let apiKey = "97f8e93f00107773f88eafd933ce86b7";
 
@@ -18,12 +20,35 @@ function load() {
   cityInput = document.querySelector(".search");
   currentCity = document.querySelector(".current-city");
   currentTemp = document.querySelector(".current-temp");
-  // cityLoc = document.querySelector(".current-city");
 
   search.addEventListener("submit", searchingCity);
   tempC.addEventListener("click", changeTempC);
   tempF.addEventListener("click", changeTempF);
   currentLocation.addEventListener("click", addCity);
+
+  let currentDate = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let weekDay = days[currentDate.getDay()];
+  currentWeekDay = document.querySelector(".current-day");
+  currentWeekDay.innerHTML = weekDay;
+  let hours = currentDate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  currentTime = document.querySelector(".current-time");
+  currentTime.innerHTML = `${hours}:${minutes}`;
 }
 
 function getCurrentTemp() {
@@ -34,8 +59,40 @@ function getCurrentTemp() {
 function setCurrentTemp(response) {
   const currentTemperature = Math.round(response.data.main.temp);
   currentTemp.innerHTML = `${currentTemperature}`;
-  let time = response.data.dt;
-  console.log(time);
+
+  // setDay(response.data.dt * 1000);
+  // setTime(response.data.dt * 1000);
+
+  function setTime(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    timeValue = `${hours}:${minutes}`;
+    currentTime = document.querySelector(".current-time");
+    currentTime.innerHTML = timeValue;
+  }
+
+  function setDay(timestamp) {
+    let date = new Date(timestamp);
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let weekDay = days[date.getDay()];
+    currentWeekDay = document.querySelector(".current-day");
+    currentWeekDay.innerHTML = weekDay;
+  }
 }
 
 function changeTempC(event) {
@@ -66,11 +123,44 @@ function getTempForMyLoc(position) {
   function setData(response) {
     let temperature = Math.round(response.data.main.temp);
     currentTemp.innerHTML = temperature;
+
     let cityLoc = response.data.name;
-    console.log(cityLoc);
     cityValue = cityLoc;
-    console.log(cityValue);
     currentCity.innerHTML = cityValue;
+
+    // setDay(response.data.dt * 1000);
+    // setTime(response.data.dt * 1000);
+
+    function setTime(timestamp) {
+      let date = new Date(timestamp);
+      let hours = date.getHours();
+      if (hours < 10) {
+        hours = `0${hours}`;
+      }
+      let minutes = date.getMinutes();
+      if (minutes < 10) {
+        minutes = `0${minutes}`;
+      }
+      timeValue = `${hours}:${minutes}`;
+      currentTime = document.querySelector(".current-time");
+      currentTime.innerHTML = timeValue;
+    }
+
+    function setDay(timestamp) {
+      let date = new Date(timestamp);
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      let weekDay = days[date.getDay()];
+      currentWeekDay = document.querySelector(".current-day");
+      currentWeekDay.innerHTML = weekDay;
+    }
   }
   axios.get(`${apiUrl}&appid=${apiKey}`).then(setData);
 }
@@ -80,62 +170,12 @@ function addCity(event) {
   navigator.geolocation.getCurrentPosition(getTempForMyLoc);
 }
 
-// week day and time - location
-let currentDate = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let weekDay = days[currentDate.getDay()];
-let currentWeekDay = document.querySelector(".current-day");
-currentWeekDay.innerHTML = weekDay;
-let hours = currentDate.getHours();
-let minutes = currentDate.getMinutes();
-let currentTime = document.querySelector(".current-time");
-currentTime.innerHTML = `${hours}:${minutes}`;
-
-// // Farenheit and Celsius
-
-// function changeTempC(event) {
-//   event.preventDefault();
-//   function getCurrentTempC(response) {
-//     let currentTemperature = Math.round(response.data.main.temp);
-//     console.log(currentTemperature);
-//     let currentTemp = document.querySelector(".current-temp");
-//     currentTemp.innerHTML = `${currentTemperature}`;
-//   }
-//   axios.get(`${apiUrlSearch}&appid=${apiKey}`).then(getCurrentTempC);
-// }
-// let tempC = document.querySelector(".current-temp-C");
-// tempC.addEventListener("click", changeTempC);
-
-// function changeTempF(event) {
-//   event.preventDefault();
-
-//   let apiKeyF = "97f8e93f00107773f88eafd933ce86b7";
-//   let apiUrlSearchF = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}units=imperial`;
-//   function getCurrentTempF(response) {
-//     let currentTemperature = Math.round(response.data.main.temp);
-//     console.log(currentTemperature);
-//     let currentTemp = document.querySelector(".current-temp");
-//     currentTemp.innerHTML = `${currentTemperature}`;
-//   }
-//   axios.get(`${apiUrlSearchF}&appid=${apiKeyF}`).then(getCurrentTempF);
-// }
-// let tempF = document.querySelector(".current-temp-F");
-// tempF.addEventListener("click", changeTempF);
-
 // to do:
 //1. F and C - DONE
-//2. change the time if minutes<10
-//3. week day and time for city that is not user location
+//2. change the time if minutes<10 - Done
+//3. week day and time for city that is not user location - only user location
 //4. images
 //5. forecast for next 5 days
 //Capital letter of the city - done
 //6. add current temp if button search is using - DONE
-// new problem: use location, than search city and in F will use user's location info again
+// new problem: use location, than search city and in F will use user's location info again - DONE
